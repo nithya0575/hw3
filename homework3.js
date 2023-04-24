9ece76892177b67f60bba28bb850d5f9347d0296
nithya0575 <nramankulangara@gmail.com>
2023-04-23 23:18:21 -0500

    testing 1

--- homework3.js
+++ homework3.js
@@ -1,3 +1,5 @@
+let error_flag = 0;
+
 const date = Intl.DateTimeFormat("en-US", {
   month: "long",
   day: "numeric",
@@ -13,6 +15,91 @@ const budgetOutput = document.getElementById("budgetValue");
 budgetSlider.oninput = function () {
   budgetOutput.innerText = "$" + budgetSlider.value;
 };
+
+function checkfirstname()
+    {
+        x = document.getElementById("firstname").value;
+        if( x.length<2) { 
+              document.getElementById("name_message").innerHTML = "Invalid first name...too short.";  
+              error_flag = 1;
+        }
+        else {
+            if (x.match(/[a-zA-Z3-5'-]+$/)) {
+              document.getElementById("name_message").innerHTML = "";  
+            }
+            else  {
+              document.getElementById("name_message").innerHTML = "Invalid characters in first name.";
+              error_flag = 1;
+              }
+        }
+    }
+    function checkmiddlename()
+    {
+        x = document.getElementById("middlename").value;
+        if( x.length>0) { 
+              if (x.match(/[a-zA-Z ]/)) {
+              document.getElementById("name_message").innerHTML = "";  
+            }
+            else  {
+              document.getElementById("name_message").innerHTML = "Invalid characters in middle name.";
+              error_flag = 1;
+              }
+        }
+    }
+function checklastname()
+    {
+        x = document.getElementById("lastname").value;
+        if( x.length<2) { 
+              document.getElementById("name_message").innerHTML = "Invalid Last Name...too short.";
+              error_flag = 1;  
+        }
+        else {
+            if (x.match(/[a-zA-Z3-5'-]+$/)) {
+              document.getElementById("name_message").innerHTML = "";  
+            }
+            else  {
+              document.getElementById("name_message").innerHTML = "Invalid characters in last name.";
+              error_flag = 1;
+              }
+        }
+    }
+
+    function checkaddress1() {
+      x = document.getElementById("address1").value;
+      console.log(x.value);
+      console.log(x.length);
+      if (x.length < 2 ) {  
+        document.getElementById("address1_message").innerHTML = "Enter something on address 1 line";  
+        error_flag = 1; 
+        console.log(error_flag);
+        }
+        else { 
+            document.getElementById("address1_message").innerHTML = "";  
+        }
+        console.log(address1_message);
+  }
+  function checkaddress2() {}
+  
+  function checkcity() {
+           if (document.getElementById("city").value.match(/^[ a-zA-Z3-5'-]+$/)) {
+                document.getElementById("city_message").innerHTML = "";  
+              }
+              else  {
+                document.getElementById("city_message").innerHTML = "Invalid characters in City name.";
+                error_flag = 1;
+                }
+  }
+  function checkstate() {
+          z=document.getElementById("state").value;
+          if(z=="") { 
+                document.getElementById("state_message").innerHTML = "Please choose a state";  
+                error_flag = 1;
+          }
+          else {
+            document.getElementById("state_message").innerHTML = ""; 
+          }
+  }
+
 
 function getDataReview() {
   const contents = document.getElementById("registration");
